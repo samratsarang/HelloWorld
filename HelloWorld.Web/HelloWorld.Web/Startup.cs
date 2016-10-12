02d1d3562e11ccc7a5ecf3a4cf74dfbe6818e84d
samratsarang <samrat.sarang@gmail.com>
2016-10-12 19:40:48 +0530

    Added MVC application

---
+++ HelloWorld.Web/HelloWorld.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloWorld.Web.Startup))]
+namespace HelloWorld.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
